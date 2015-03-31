b4397f005e8cd95c878632788e5bd1e3ad57ed74
Rodel Rebucas <rodelrebucas@gmail.com>
2015-03-31 23:56:13 +0800

    input syntax

---
+++ rodel/user_input.py
@@ -0,0 +1,10 @@
+# user_input.py
+
+print("Enter integer: ")
+i = input()
+print("You've entered",i)
+print(type(i))
+x = input("Enter value of x")
+print(x)
+x = int(input("prompt"))
+print(type(x))
