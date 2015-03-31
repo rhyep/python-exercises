02862129949f1123d81ee77ef38e3ff40402c9d2
Van Victor Lim <vanvictorlim@gmail.com>
2015-03-31 23:54:25 +0800

    added user input

---
+++ van/user_input.py
@@ -0,0 +1,11 @@
+# user_input
+
+
+print("Enter integer:")
+i = input()
+print("You've entered", i)
+print(type(i))
+x = input("Enter value of x")
+print(x)
+x = int(input("prompt"))
+print(type(x))
