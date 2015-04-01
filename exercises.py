5e013149e84b67d6b1fa3cef35bf01e249cb707a
Jan Maghuyop <jangmaghuyop@gmail.com>
2015-04-01 22:53:45 +0800

    made summary 20 2

--- exercises.py
+++ exercises.py
@@ -1,4 +1,4 @@
-# Comments
+# 1 comments
 
 # This is a single line comment.
 """
@@ -8,7 +8,7 @@ This is a multiple line comment.
 """
 
 
-# Values and Variable
+# 2 values and variable
 print("I am a string") 	# String
 string = "String 2"
 print(string)
@@ -16,24 +16,24 @@ print(7)				# Integer
 y = 2 					# assignment
 print(y) 				# variable
 
-# valid identifiers
+# 3 valid identifiers
 x2 = 1
 total = 3
 port_80 = 80
 
-# Float
+# 4 float
 f = 21.21
 ff = 21.212121
 print("%.2f" % f)
 print("%.3f" % ff)
 
-# type
+# 5 type
 print(type(4))
 f = 20.1
 print(type(f))
 print(type("Hey!"))
 
-# multiple assignment
+# 6 multiple assignment
 a = 20
 a = 30
 a = 40
@@ -41,7 +41,7 @@ print(a)
 a, b, c = 10, 20, 30
 print(a, b, c)
 
-# round
+# 7 round
 a = 2.1
 b = round(a)
 print(a)
@@ -53,19 +53,19 @@ print(aa)
 b = round(aa, 3)
 print(b)
 
-# string control codes
+# 8 string control codes
 print("A\nB\nC")
 print("D\tE\tF")
 
 
-# escape quotes
+# 9 escape quotes
 print("'single'")
 print('"double"')
 print('\'singlescape\'')
 print("\"doublescape\"")
 
 
-# user input
+# 10 user input
 print("Enter integer: ")
 i = input()
 print("You've entered ", i)
@@ -74,9 +74,11 @@ x = input("Enter value of x")
 print(x)
 
 
-# print function
+# 11 print function
 print("end with end", end="")
 print("end with newline", end="\n")
+print("end with nothing", end="")
+print("end with tab", end="\t")
 
 x, y, z = 1, 2, 3
 print(x, y, z)
@@ -84,72 +86,100 @@ print(x, y, z, sep="-")
 
 print('{0} {1}'.format(0, 10 ** 0))
 print('{0} {1}'.format(1, 10 ** 1))
-print('{0} {1}'.format(2, 10 ** 2))
-print('{0} {1}'.format(3, 10 ** 3))
+print('{0} {0}'.format(2, 10 ** 2))
+print('{1} {1}'.format(3, 10 ** 3))
 print('{0} {1}'.format(4, 10 ** 4))
+print('{1} {1}'.format(4, 4))
 
 
-# arithmetic expressions
+# 12 arithmetic expressions
 a = 1
 b = 2
 c = a + b
 d = a - b
 e = a * b
 f = a / b
-g = a // b
+g = a // b # floor division
 h = a % b
 i = a ** b
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+print(i)
 
-# mix type
+# 13 mix type
 a = 10
 b = 2.2
 c = a + b
+print(c)
 
-# precedence
+
+# 14 precedence
+a = 1
+b = 2
+c = 3
 d = (c + a) * b
 e = c + a * b
+f = a + a * b + c
+h = a + b**b + (a + b)
 
-# other
+
+# 15 compound operators
 x = 1
 y = 2
 x += y
-x +- y
+y -= x
+print(x, y)
 
-# conditinal and boolean expressions
-True
-False
-type(True)
-type(False)
+
+# 16 boolean expressions
+print(True)
+print(False
+print(type(True))
+print(type(False))
+
+
+# 17 if statement
 
 a = 1
 b = 2
 
-# if statement
 if a > 1:
 	print("a is >")
 if a == 1:
 	print("a == 1")
-if a <= 1:
-	print("a == 1")
-if a < 1:
-	print("a < 1")
+if b <= 1:
+	print("b <= 1")
+if b < 1:
+	print("b < 1")
 
-# else
+
+# 18 else
 if a == 1:
 	print("a == 1")
 else:
 	print("a != 1")
 
-# compound boolean
+
+# 19 compound boolean, operators
+a = 1
+b = 2
+
 if a == b and b == 0:
 	print("a = b, b = 0")
-if a == b or b == 0:
-	print("or")
-if a == b not b == 0:
-	print("not b = 0")
+else:
+	print("and!")
+if a == b or b == 2:
+	print("or!")
+
+if a == b or not b == 2:
+	print("or!")
 
 
-# pass
+# 20 pass
 a = 10
 b = 20
 if a > b:
@@ -157,8 +187,14 @@ if a > b:
 else:
 	print("else")
 
+if a < b:
+	pass # do nothing
+else:
+	print("else")
 
-# elif
+
+
+# 21 elif
 a = "a"
 if a == "x":
 	print("x")
---
+++ jan/summary_20_2.py
@@ -0,0 +1,104 @@
+# summary_20_2.py
+
+"""
+Write a program that accepts the name of a tax payer,
+"""
+name = input("Tax Payer : ")
+
+"""
+accepts the current assessed value of the building and land,
+"""
+print("\nAssessed value")
+assessed_value_bldg = float(input("Building : "))
+assessed_value_land = float(input("Land : "))
+
+"""
+compute the basic tax by 1 percent of the assessed value both building and land.
+then compute both it's annual tax
+then compute the total annual tax
+"""
+
+annual_basic_tax_land = (assessed_value_land * .01) * 12
+annual_basic_tax_bldg = (assessed_value_bldg * .01) * 12
+
+total_annual_tax = annual_basic_tax_land + annual_basic_tax_bldg
+
+
+"""
+if the assessed value of the land is greater than or equal to 50,000
+	compute additional tax of 20 percent of his land assessed value.
+"""
+
+
+
+"""
+accepts the current month in integer.
+"""
+month = int(input("\n\nEnter month in integer : "))
+
+
+
+"""
+if current month is march (3) and  below or before
+	compute for the discount (20 percent of annual_basic_tax both building and land)
+else
+	compute for the penalty (20 percent of annual_basic_tax both building and land).
+"""
+
+if month <= 3:
+	discount_land = annual_basic_tax_land * .20
+	discount_bldg = annual_basic_tax_bldg * .20
+
+	total_discount = discount_land + discount_bldg
+
+	annual_land_tax = annual_basic_tax_land - discount_land
+	annual_bldg_tax = annual_basic_tax_bldg - discount_bldg
+
+	total_annual_tax = annual_land_tax + annual_bldg_tax
+
+	penalty_land = 0
+	penalty_bldg = 0
+	total_penalty = 0
+
+else:
+	penalty_land =  annual_basic_tax_land * .20
+	penalty_bldg = annual_basic_tax_bldg * .20
+
+	total_penalty = penalty_land + penalty_bldg
+
+	annual_land_tax = annual_basic_tax_land + penalty_land
+	annual_bldg_tax = annual_basic_tax_bldg + penalty_bldg
+
+	total_annual_tax = annual_land_tax + annual_bldg_tax
+
+	discount_land = 0
+	discount_bldg = 0
+	total_discount = 0
+
+
+if assessed_value_land >= 50000:
+	additional_tax = assessed_value_land * .2
+	total_annual_tax += additional_tax
+else:
+	additional_tax = 0
+
+print("\nAdditional tax : %.2f\n" % additional_tax)
+
+print("Total")
+print("Annual tax : %.2f" % total_annual_tax)
+print("Discount : %.2f" % total_discount)
+print("Penalty : %.2f" % total_penalty)
+
+print()
+
+print("Building")
+print("Annual tax : %.2f" % annual_bldg_tax)
+print("Discount : %.2f" % discount_bldg)
+print("Penalty : %.2f" % penalty_bldg)
+
+print()
+
+print("Land")
+print("Annual tax : %.2f" % annual_land_tax)
+print("Discount : %.2f" % discount_land)
+print("Penalty : %.2f" % penalty_land)
