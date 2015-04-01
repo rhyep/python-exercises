e081a23c7ea95d0ae65f59a810ada4db65d73f42
Rodel Rebucas <rodelrebucas@gmail.com>
2015-04-01 15:08:37 +0800

    added spaces

--- rodel/print_function.py
+++ rodel/print_function.py
@@ -8,16 +8,8 @@ x, y, z = 1, 2, 3
 print(x, y, z)
 print(x, y, z, sep="-")
 
-<<<<<<< HEAD
 print('{0} {1}'.format(0, 10**0))
 print('{0} {1}'.format(1, 10**1))
 print('{0} {1}'.format(2, 10**2))
 print('{0} {1}'.format(2, 10**3))
 print('{1} {1}'.format(100, 200))
-=======
-print('{0} {1}'.format(0,10**0))
-print('{0} {1}'.format(1,10**1))
-print('{0} {1}'.format(2,10**2))
-print('{0} {1}'.format(2,10**3))
-print('{1} {1}'.format(100,200))
->>>>>>> bf7cdec8ff9b539410904c64b21347839bbd0694
