273d25b5383307ca18b79e80050f5a2f95c8c5ea
Rodel Rebucas <rodelrebucas@gmail.com>
2015-04-02 20:27:55 +0800

    done fizzbuzz

--- rodel/summary_27.py
+++ rodel/summary_27.py
@@ -39,26 +39,24 @@ or equak to 5 else display:
 0
 
 """
-
+"""
 from random import randrange
 
-"""
+
 def sq(x):
     for i in range(x):
         for j in range(x):
             print("0",end="")
         print(end="\n")
 
-"""
-"""
+
 def tr(x):
 
     for i in range(x+1):
         for j in range(i):
             print("0",end="")
         print(end="\n")
-"""
-"""
+
 def rt(x):
 
     for i in range(x):
@@ -66,15 +64,15 @@ def rt(x):
             print("0",end="")
         x-=1
         print(end="\n")
-"""
-"""
+
 def ra(x):
 
     for i in range(x):
         for i in range(randrange(1,x)):
             print("0",end="")
         print(end="\n")
-"""
+
+
 def arr(x):
     for i in range(x):
         for j in range(i):
@@ -83,13 +81,22 @@ def arr(x):
         print("0")
 
 """
-def main(x):
-    while continue="no":
+def fizzbuzz(x):
+
+    for i in range(1,x+1):
+        if i%3==0:
+            print(i," = Fizz")
+        elif i%5==0:
+            print(i," = Buzz")
+        elif  i%3==0 and i%5==0:
+            print(i," = fizzbuzz")
+        else:
+            print(i," = FizzyBuzzy")
 
 
-"""
+
 
 
 
 a=int(input("input sq: "))
-arr(a)
+fizzbuzz(a)
