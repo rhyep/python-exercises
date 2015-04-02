297f9ab9ec075f245fd0635f1b0f551eef0f9cf8
Rodel Rebucas <rodelrebucas@gmail.com>
2015-04-02 18:26:09 +0800

    for loop exercises

---
+++ rodel/for_loop.py
@@ -0,0 +1,15 @@
+# for_loop.py
+
+# print 1 to 100
+for n in range(1, 101):
+    print(n)
+
+
+# 3rd paramater step
+for n in range(1, 100, 2):
+    print(n)
+
+
+# decrement
+for n in range(100, 0, -1):
+    print(n)
