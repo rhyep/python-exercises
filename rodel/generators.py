7e18f3e93489e81b5d3435b9981e83a0e1beac77
Rodel Rebucas <rodelrebucas@gmail.com>
2015-04-03 17:28:49 +0800

    file_objects

---
+++ rodel/generators.py
@@ -0,0 +1,39 @@
+# generators.py
+
+count = 0
+
+
+def remember():
+    global count  # set count as global variable
+    count += 1
+    print(count)
+
+remember()
+remember()
+remember()
+remember()
+remember()
+remember()
+
+
+# yield
+
+def gens():
+    yield 3
+    yield "wow"
+    yield 1.3
+    yield -4
+
+#print type
+print(gens)
+print(type(gens))
+print(gens())
+x = gens()
+y = gens()
+print(type(x))
+print(x)
+print(x, y)
+print(next(x))
+print(next(x))
+print(next(x))
+print(next(x))
