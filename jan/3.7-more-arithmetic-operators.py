2bc6236ed9475039eaf4be356cde2dba2744e3a3
Jan Maghuyop <jangmaghuyop@gmail.com>
2015-04-04 16:53:02 +0800

    more arithmetic operators

---
+++ jan/3.7-more-arithmetic-operators.py
@@ -0,0 +1,21 @@
+# 3.7-more-arithmetic-operators.py
+
+x = x + 1
+x -= 1
+print(x)
+
+x = x + 5
+x += 5
+print(x)
+
+# x op= exp
+
+# x is a variable.
+# op= is an arithmetic operator combined with the assignment operator;
+for our purposes, the ones most
+# useful to us are +=, -=, *=, /=, //=, and %=.
+# exp is an expression compatible with the variable x.
+
+x *= y + z
+# equal
+x = x * (y + z)
