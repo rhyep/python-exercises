99e6ad3d6698a0aa67dbad6200585deebe9e138e
Jan Maghuyop <jangmaghuyop@gmail.com>
2015-04-04 16:02:04 +0800

    user input

---
+++ jan/2.6-user-input.py
@@ -0,0 +1,25 @@
+# 2.6-user-input.py
+
+print('Input a string : ')
+
+i = input()
+
+print('Entered string is', i)
+
+print(type(i))
+
+# input integer
+print('Integer : ')
+a = input()
+
+print('Integer : ')
+b = input()
+
+n1 = int(a)
+n2 = int(b)
+
+print(n1, '+', n2, '=', n1 + n2)
+
+# prompt string as a parameter
+c = input('Input string : ')
+print(type(c))
