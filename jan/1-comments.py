47b5e78aa9967364fc593d93658f6257b62d63a8
Jan Maghuyop <jangmaghuyop@gmail.com>
2015-04-04 13:19:05 +0800

    made 1 and 2

---
+++ jan/1-comments.py
@@ -0,0 +1,18 @@
+# 1-comments.py
+
+# This is a single line comment.
+
+"""
+This is a multiple line comment.
+This is a comment.
+Comment.
+"""
+
+'''
+This is also a multiple line comment.
+This is also a comment.
+Comment.
+print("This should not work!")
+'''
+
+print("It works!")
---
+++ jan/2-values-and-variables.py
@@ -0,0 +1,15 @@
+# 2-values-and-variables.py
+
+print("I am a string!")
+
+# assignment
+string = "This is a string also!"
+
+print(string)
+
+# Integer
+print(7)
+
+seven = 7
+
+print(seven)
