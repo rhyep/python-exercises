1550450d54cc9e256d63dda4b9bd12de46baee03
Jan Maghuyop <jangmaghuyop@gmail.com>
2015-04-04 15:56:02 +0800

    string control codes

---
+++ jan/2.5-control-codes-within-strings.py
@@ -0,0 +1,21 @@
+# 2.5-control-codes-within-strings.py
+
+# newline
+print('X\nY\nZ')
+
+# tab
+print('X\tY\tZ')
+
+# single qoutes
+print("buzz 'buzz'")
+
+
+# double quotes
+print('fizz "fizz"')
+
+# escape symbol
+print("fizz \"fizz\"")
+print('buzz \'buzz\'')
+
+# path escape symbol
+path = 'C:\\path\\to\\file'
