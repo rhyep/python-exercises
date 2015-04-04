68803619c996dd595ac87579a24fa538dbee639f
Jan Maghuyop <jangmaghuyop@gmail.com>
2015-04-04 16:57:06 +0800

    boolean expressions

---
+++ jan/4.1-boolean-expressions.py
@@ -0,0 +1,26 @@
+# 4.1-boolean-expressions.py
+
+a = True
+b = False
+
+print(a)
+print(b)
+
+print(type(a))
+print(type(b))
+
+'''
+x == y      True if x = y (mathematical equality, not assignment);
+            otherwise, false
+x < y       True if x < y; otherwise, false
+x <= y      True if x ≤ y; otherwise, false
+x > y       True if x > y; otherwise, false
+x >= y      True if x ≥ y; otherwise, false
+x != y      True if x = y; otherwise, false
+'''
+
+print(10 < 20)
+print(10 >= 20)
+x = 10
+print(x < 100)
+print(x != y)
