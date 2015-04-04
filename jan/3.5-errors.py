fbf3fc68345001a9c233c586e93709d9bf40accd
Jan Maghuyop <jangmaghuyop@gmail.com>
2015-04-04 16:44:25 +0800

    made erros

---
+++ jan/3.5-errors.py
@@ -0,0 +1,36 @@
+# 3.5-errors.py
+
+# syntax errors
+4 + 2 = x
+
+x = 4 + 2
+print(x)
+
+
+# parenthesis
+x = )3 + 4)
+
+# mismatch
+x = 'hello"
+
+# identation
+x = 2
+ y = 5  # wrongly indented
+
+# run time exceptions
+x = y + 2  #  y is not defined
+
+# Get two integers from the user
+dividend, divisor = eval(input('Please enter two numbers to divide: '))
+
+# Divide them and report the result
+print(dividend, '/', divisor, "=", dividend/divisor)
+
+# Get a number from the user
+value = eval(input('Please enter a number to cut in half: '))
+# Report the result
+print(value/2)
+
+# logic errors
+# divisor / dividend
+# dividend/divisor
