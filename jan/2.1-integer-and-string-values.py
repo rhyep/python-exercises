518253c1666053fd193e45edc88d18ac68efd2b3
Jan Maghuyop <jangmaghuyop@gmail.com>
2015-04-04 13:42:28 +0800

    new

---
+++ jan/2.1-integer-and-string-values.py
@@ -0,0 +1,31 @@
+# 2.1-integer-and-string-values.py
+
+# Integer value
+print(8)
+
+print(2 + 5 + 3)
+
+# String value
+print('4')
+
+print('name')
+
+print("names")
+
+# type, class?
+print(type(6))
+
+print(type('6'))
+
+# convert string to integer
+print(int('5'))
+
+# convert integer to string
+print(str(240))
+
+print(5 + 10)
+
+# string concatination
+print('5' + '10')
+
+print(5 + int('5'))
